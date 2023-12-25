bcbf21692757c8782527344c1ec5b6eb765e660e
fernandorsdiniz81 <137434584+fernandorsdiniz81@users.noreply.github.com>
2023-12-25 20:59:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,41 +7,42 @@ class TelegramBot():
 		response = requests.get(init_url)
 		status_code = response.status_code
 		response = response.json() # recebe todas as mensagens do chat
-		# print(status_code, response.keys()) # teste
-		# print(response['result'])# teste
+		# print(response) # teste
 		if 'result' in response:
 			amount_of_messages = len(response['result'])
 			i = amount_of_messages - 1 # identifica o indice da última mensagem
 			try:
 				self.update_id = response['result'][i]['update_id'] # captura o update_id da última mensagem
+				self.chat_id = response['result'][0]['message']['from']['id'] # captura o chat_id (quem é o cliente)
 			except:
-				# self.update_id = response['result'][0]['update_id'] # caso não haja mensagens anteriores
-				pass
-			self.chat_id = response['result'][i]['message']['from']['id'] # captura o chat_id (quem é o cliente)
-			self.text = ""
-					
+				self.update_id = response['result'][0]['update_id'] # caso não haja mensagens anteriores
+				self.chat_id = response['result'][0]['message']['from']['id'] # captura o chat_id (quem é o cliente)
+		self.last_answered_message = None
+
 	def read_messages(self):
 		# print(f"\nID antes: {self.update_id}")
 		read_message_url = f'https://api.telegram.org/bot{self.token}/getUpdates?timeout=100&offset={self.update_id}'
 		response = requests.get(read_message_url)
-		response = response.json()		
+		response = response.json()
 		if 'result' in response:
-			self.text = response['result'][0]['message']['text']
-			# print(self.text)
-			#message_id = response['result'][0]['message']['message_id'] # sequencial de mensagens do cliente
-			self.update_id += 1
-			# print(f"ID depois:{self.update_id}")
+			try: # importante, pois após o timeout, se não houver mensagem nova, haverá erro na request com o update_id que foi incrementado.
+				self.text = response['result'][0]['message']['text'] # o índice sempre será 0 pq a request é pelo update_id
+				# print(self.text)
+				self.message_id = response['result'][0]['message']['message_id'] # sequencial de mensagens do cliente
+				self.update_id += 1
+				# print(f"ID depois:{self.update_id}")
+			except:
+				pass # não executa nada em caso de erro pq a função está no laço while
 
 	def answer_messages(self):
-		answer = f'Ola!! :) Você disse "{self.text}"!'
-		url_para_responder = f'https://api.telegram.org/bot{self.token}/sendMessage?chat_id={self.chat_id}&text={answer}'
-		requests.get(url_para_responder)
+		if self.last_answered_message != self.message_id:
+			answer = f'Ola!! :) Você disse "{self.text}"!'
+			url_para_responder = f'https://api.telegram.org/bot{self.token}/sendMessage?chat_id={self.chat_id}&text={answer}'
+			requests.get(url_para_responder)
+			self.last_answered_message = self.message_id
 
 
 bot = TelegramBot()
 while True:
 	bot.read_messages()
 	bot.answer_messages()
-
-
-
