35d076852293300f4c000ce50332af1df129f0aa
fernandorsdiniz81 <137434584+fernandorsdiniz81@users.noreply.github.com>
2023-12-25 21:16:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,24 +15,27 @@ class TelegramBot():
 				self.update_id = response['result'][i]['update_id'] # captura o update_id da última mensagem
 				self.chat_id = response['result'][0]['message']['from']['id'] # captura o chat_id (quem é o cliente)
 			except:
-				self.update_id = 0
-				self.chat_id = 0
+				self.update_id = response['result'][0]['update_id'] # captura o update_id da última mensagem
+				self.chat_id = response['result'][0]['message']['from']['id'] # captura o chat_id (quem é o cliente)
 		self.last_answered_message = None
 
 	def read_messages(self):
 		# print(f"\nID antes: {self.update_id}")
-		read_message_url = f'https://api.telegram.org/bot{self.token}/getUpdates?timeout=100&offset={self.update_id}'
-		response = requests.get(read_message_url)
-		response = response.json()
-		if 'result' in response:
-			try: # importante, pois após o timeout, se não houver mensagem nova, haverá erro na request com o update_id que foi incrementado.
-				self.text = response['result'][0]['message']['text'] # o índice sempre será 0 pq a request é pelo update_id
-				# print(self.text)
-				self.message_id = response['result'][0]['message']['message_id'] # sequencial de mensagens do cliente
-				self.update_id += 1
-				# print(f"ID depois:{self.update_id}")
-			except:
-				pass # não executa nada em caso de erro pq a função está no laço while
+		try:
+			read_message_url = f'https://api.telegram.org/bot{self.token}/getUpdates?timeout=100&offset={self.update_id}'
+			response = requests.get(read_message_url)
+			response = response.json()
+			if 'result' in response:
+				try: # importante, pois após o timeout, se não houver mensagem nova, haverá erro na request com o update_id que foi incrementado.
+					self.text = response['result'][0]['message']['text'] # o índice sempre será 0 pq a request é pelo update_id
+					# print(self.text)
+					self.message_id = response['result'][0]['message']['message_id'] # sequencial de mensagens do cliente
+					self.update_id += 1
+					# print(f"ID depois:{self.update_id}")
+				except:
+					pass # não executa nada em caso de erro pq a função está no laço while
+		except:
+			pass
 
 	def answer_messages(self):
 		if self.last_answered_message != self.message_id:
