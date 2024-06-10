e3babbac34cd1911335c24604f3c921393f919fa
fernandorsdiniz81 <137434584+fernandorsdiniz81@users.noreply.github.com>
2024-06-10 06:54:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import ai_engine
 import requests
 import os
 from dotenv import load_dotenv
+import menu
 
 
 class TelegramBot:
@@ -10,7 +11,9 @@ class TelegramBot:
 		self.token = os.environ["TOKEN"] # token do bot do Telegram
 		self.timeout = timeout
 		self.initial_url = f"http://api.telegram.org/bot{self.token}/getUpdates?timeout={self.timeout}"
+		self.user_names = dict()
 		self.bot_intelligence = ai_engine.AIEngine()
+		self.menu = menu.Menu()
 
 
 	def get_update_id(self) -> bool: # esta função verifica se há mensagem nova (update_id)
@@ -21,8 +24,6 @@ class TelegramBot:
 			try:
 				i = len(response["result"]) - 1
 				self.update_id = response["result"][i]["update_id"] # último "update_id" do json
-				self.first_name = response["result"][0]["message"]["from"]["first_name"] # primeiro nome do cliente
-				self.chat_id = str(response["result"][0]["message"]["from"]["id"]) # quem é o cliente - identificador único do chat
 				return True
 			except Exception as error:
 				print(error)
@@ -34,12 +35,14 @@ class TelegramBot:
 			response = requests.get(read_message_url)
 			response = response.json()
 			self.update_id = response["result"][0]["update_id"] # sequencial da mensagem do bot, cada "update-id" é uma mensagem, do mesmo cliente ou não
-			# self.chat_id = str(response["result"][0]["message"]["from"]["id"]) # quem é o cliente - identificador único do chat
-			# self.first_name = response["result"][0]["message"]["from"]["first_name"] # primeiro nome do cliente
+			self.chat_id = str(response["result"][0]["message"]["from"]["id"]) # quem é o cliente - identificador único do chat
+			self.first_name = response["result"][0]["message"]["from"]["first_name"] # primeiro nome do cliente
 			self.message_id = response["result"][0]["message"]["message_id"] # sequencial de mensagens do cliente
 			self.human_message = response["result"][0]["message"]["text"] # mensagem do cliente
-			self.update_id += 1 # para tentar buscar a próxima mensagem
-			print(f"update_id: {self.update_id}\nfirst_name: {self.first_name}\nchat_id: {self.chat_id}\nmessage_id: {self.message_id}\n\nhuman: {self.human_message}\n")
+			self.user_names[self.chat_id] = self.first_name
+			self.bot_intelligence.answer_messages_with_ai(self.chat_id, f"Meu nome é {self.user_names.get(self.chat_id)}.")
+			self.update_id += 1 # incrementa update_id a fim de buscar a próxima mensagem (por isso este bloco está dentro de "try")
+			print(f"update_id: {self.update_id}\nfirst_name: {self.first_name}\nchat_id: {self.chat_id}\nmessage_id: {self.message_id}\n\n{self.user_names.get(self.chat_id)}: {self.human_message}\n")
 			return True
 		except:
 			pass # caso não haja mensagem com "update_id+1", o laço "while" continuará buscando pelo mesmo "update_id+1" até encontrar
@@ -50,16 +53,26 @@ class TelegramBot:
 		answer_content = answer.content
 		answer_message_url = f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={self.chat_id}&text={answer_content}"
 		requests.get(answer_message_url)
-		print(f"bot: {answer_content}")
+		print(f"bot para {self.user_names.get(self.chat_id)}: {answer_content}")
+
+
+	def answer_menu_messages(self): # este método trata a interação com o módulo "menu"
+		options =  self.menu.show_menu()
+		answer_menu_url = f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={self.chat_id}&text={options}"
+		requests.get(answer_menu_url)
+		print(f"bot: {options}")
+
 
 
 	def run_bot(self) -> None:
 		while True:
 			if self.get_update_id(): # quando encontrado um update_id, significa que chegou a primeira mensagem e chat inicia
 				break 
-		self.bot_intelligence.answer_messages_with_ai(self.chat_id, f"Meu nome é {self.first_name}.") # -> LangChain object
 		while True:
 			if self.read_messages():
+				# if message.strip().lower() == "menu":
+				# 	self.answer_menu_messages()
+				# else:
 				self.answer_messages()
 				
 
